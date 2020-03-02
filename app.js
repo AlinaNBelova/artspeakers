7ceaedef0f83d4cede3b3fa2ab7aa679e7e84b53
Veronica <veronica@digitalcrafts.com>
2020-03-02 07:33:43 -0600

    day 1 of express project

--- app.js
+++ app.js
@@ -1,33 +1,15 @@
-var express = require('express');
-var reload = require('reload');
-var app = express();
-var dataFile = require('./data/data.json');
-var io = require('socket.io')();
+let express = require('express');
+let app = express();
+let port = 3000;
 
-app.set('port', process.env.PORT || 3001 );
-app.set('appData', dataFile);
+
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.locals.siteTitle = 'Roux Meetups';
-app.locals.allSpeakers = dataFile.speakers;
+app.use(require('./routes/index'))
+app.use(require('./routes/speakers'))
 
-app.use(express.static('public'));
-app.use(require('./routes/index'));
-app.use(require('./routes/speakers'));
-app.use(require('./routes/feedback'));
-app.use(require('./routes/api'));
-app.use(require('./routes/chat'));
-
-var server = app.listen(app.get('port'), function() {
-  console.log('Listening on port ' + app.get('port'));
-});
-
-io.attach(server);
-io.on('connection', function(socket) {
-  socket.on('postMessage', function(data) {
-    io.emit('updateMessages', data);
-  });
-});
-
-reload(server, app);
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
